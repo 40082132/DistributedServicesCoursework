8f3675018b3b5089aca32a36ba1f4f497262f1f2
40082132 <40082132@live.napier.ac.uk>
2016-03-31 03:33:07 +0100

    Registration work
    Registration work

--- DistributedServicesCW/Register.xaml.cs
+++ DistributedServicesCW/Register.xaml.cs
@@ -19,10 +19,18 @@ namespace DistributedServicesCW
     /// </summary>
     public partial class Register : Window
     {
+        string badpw;
+        int count = 0;
+        public List<string> unsafePasswordList = new List<string>();
+        System.IO.StreamReader file = new System.IO.StreamReader("worstpasswords.txt");
+        
+        
         public Register()
         {
+
             InitializeComponent();
-        }
+            
+    }
 
         private void btnBack_Click(object sender, RoutedEventArgs e)
         {
@@ -33,6 +41,25 @@ namespace DistributedServicesCW
 
         private void btnConfirm_Click(object sender, RoutedEventArgs e)
         {
+            User u1 = new User();
+            while((badpw = file.ReadLine()) != null)
+            {
+                unsafePasswordList.Add(badpw);
+            }
+            file.Close();
+            u1.EmailAddress = txtEmail.Text;
+            u1.FirstName = txtFirstName.Text;
+            u1.LastName = txtLastName.Text;
+            u1.Username = txtUsername.Text;
+            u1.Password = txtPassword.Text;
+            foreach(var p in unsafePasswordList)
+            {
+                if (u1.Password == p)
+                {
+
+                }
+            }
+
             FileStorageInterface filestore = new FileStorageInterface();
             filestore.Show();
             this.Close();
