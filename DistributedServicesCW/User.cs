398d686952af5bed860bfe0289951748a218eac0
40082132 <40082132@live.napier.ac.uk>
2016-03-31 02:53:43 +0100

    Security additions
    Added list of worst passwords for evaluation purposes

--- DistributedServicesCW/User.cs
+++ DistributedServicesCW/User.cs
@@ -6,12 +6,16 @@ using System.Threading.Tasks;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace DistributedServicesCW
 {
+    
     [DataContract(Namespace="http://www.napier.ac.uk")]
     public class User
     {
+        
         [DataMember(Name = "first_name")]
         public string FirstName;
         [DataMember(Name = "last_name")]
@@ -24,6 +28,54 @@ namespace DistributedServicesCW
         public string Password;
         [DataMember(Name = "User Number")]
         public string UserNumber;
+        public bool emailisValid()
+        {
+           
+            if (Regex.IsMatch(EmailAddress, @"((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool firstNameIsValid()
+        {
+            if (Regex.IsMatch(FirstName, @"/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool lastNameIsValid()
+        {
+            if(Regex.IsMatch(LastName, @"/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool usernameIsValid()
+        {
+            if(Regex.IsMatch(Username, @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(_|[^\w])).+$"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool passwordIsValid()
+
 
     }
+   
 }
